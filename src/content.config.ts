ab6d360ccf5a848b9c7df7608a98f2a4f43e6ed1
mssngr <gabriel.konkle@gmail.com>
2026-07-18 09:47:07 -0600

    Revamp of several things

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,33 @@
 import { defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
+import { z } from 'astro/zod'
 
 const pages = defineCollection({
-  loader: glob({ pattern: '**/*.yml', base: './site/pages' }),
+  loader: glob({ pattern: '**/*.yml', base: './src/content/pages' }),
+  schema: () =>
+    z.object({
+      heading: z.string(),
+      heading_es: z.string(),
+      content: z.string(),
+      content_es: z.string(),
+      photoAltText: z.string(),
+      photoAltText_es: z.string(),
+    }),
 })
 
 const issues = defineCollection({
-  loader: glob({ pattern: '**/*.yml', base: './site/issues' }),
+  loader: glob({ pattern: '**/*.yml', base: './src/content/issues' }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      title_es: z.string(),
+      content: z.string(),
+      content_es: z.string(),
+      link: z.string(),
+      slug: z.string(),
+      lucideIcon: z.string(),
+      order: z.number(),
+    }),
 })
 
 export const collections = { pages, issues }
